c0473e7e23868621ebaee7c14162d760577adaab
brlopez192 <brlopez192@gmail.com>
2025-02-20 02:49:17 -0500

    4.4 Tests: passed before Exercise Task

--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -15,30 +15,36 @@ const CitySearch = ({ allLocations }) => {
         setSuggestions(filteredLocations);
       };
 
+      const handleItemClicked = (event) => {
+        const value = event.target.textContent;
+        setQuery(value);
+        setShowSuggestions(false);
+      };
 
-    return (
-      <div id="city-search">
-        <input
+
+      return (
+        <div id="city-search">
+          <input
             type="text"
             className="city"
             placeholder="Search for a city"
             value={query}
             onFocus={() => setShowSuggestions(true)}
             onChange={handleInputChanged}
-        />
-              {showSuggestions ?
-        <ul className="suggestions">
-          {suggestions.map((suggestion) => {
-            return <li key={suggestion}>{suggestion}</li>
-          })}
-          <li key='See all cities'>
-            <b>See all cities</b>
-          </li>
-        </ul>
-        : null
-      }
-      </div>
-    )
+          />
+          {showSuggestions ?
+            <ul className="suggestions">
+              {suggestions.map((suggestion) => {
+                return <li onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
+              })}
+              <li key='See all cities' onClick={handleItemClicked}>
+                <b>See all cities</b>
+              </li>
+            </ul>
+            : null
+          }
+        </div>
+     )
    }
   
   
