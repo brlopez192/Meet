30ce35f37a6e1d480e863ca1733cd068c68912a0
brlopez192 <brlopez192@gmail.com>
2025-02-20 03:50:23 -0500

    exercise 4.4 Completion

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import React from "react";
 import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
+import NumberOfEvents from "./components/NumberOfEvents";
 const App = () =>{
   return (
     <>
     <div>
       <CitySearch />
       <EventList />
+      <NumberOfEvents />
     </div>
     </>
   );
