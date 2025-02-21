26bab084b689db5e14b03efdea55c5b69184572e
brlopez192 <brlopez192@gmail.com>
2025-02-21 05:15:31 -0500

    offline use

--- src/api.js
+++ src/api.js
@@ -54,21 +54,25 @@ export const getEvents = async () => {
   if (window.location.href.startsWith("http://localhost")) {
     return mockData;
   }
- 
- 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events?JSON.parse(events):[];
+  }
+
   const token = await getAccessToken();
- 
- 
   if (token) {
     removeQuery();
-    const url =  "https://8p18ehaiok.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" + "/" + token;
+    const url = "https://8p18ehaiok.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" + "/" + token;
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
- };
+};
 
  const removeQuery = () => {
   let newurl;
