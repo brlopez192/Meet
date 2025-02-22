4f9df9b23faaa896473c5816a222ad920fb6150f
brlopez192 <brlopez192@gmail.com>
2025-02-22 01:56:10 -0500

    events update

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 const Event = ({ event }) => {
   const [showDetails, setShowDetails] = useState(false);
   return (
-    <li className="event" id={event.id}>
+    <li className="event">
       <h2>{event && event.summary}</h2>
       <p>{event && event.location}</p>
       <p>{event && (new Date(event.created)).toUTCString()}</p>
