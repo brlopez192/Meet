2ca83df55ca82ae8b5c70dbcf93234a1b3eb9687
brlopez192 <brlopez192@gmail.com>
2025-02-22 02:58:35 -0500

    pie chart

---
+++ src/components/EventGenresChart.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import {
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  Cell,
+  Legend,
+  Tooltip,
+} from 'recharts';
+
+const EventGenresChart = ({ events }) => {
+  const [data, setData] = useState([]);
+  const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
+
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#A28DFF'];
+
+  useEffect(() => {
+    setData(genres.map((genre) => ({
+      name: genre,
+      value: events.filter(event => event.summary.includes(genre)).length,
+    })));
+  }, [events]);
+
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const RADIAN = Math.PI / 180;
+    const radius = outerRadius;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
+    const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
+    return percent ? (
+      <text
+        x={x}
+        y={y}
+        fill={COLORS[index % COLORS.length]} // Use matching color
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+      >
+        {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
+      </text>
+    ) : null;
+  };
+
+  return (
+    <ResponsiveContainer width="99%" height={400}>
+      <PieChart>
+        <Pie
+          data={data}
+          dataKey="value"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={130}
+        >
+          {/* Apply colors to each slice */}
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Tooltip />
+        <Legend />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default EventGenresChart;
