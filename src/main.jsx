36aa5570db8e7ca8e60748ffcad6c050046d40ab
brlopez192 <brlopez192@gmail.com>
2025-02-22 01:55:12 -0500

    events update

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration.js'
 atatus.config('6cf9b350f64047c3b49b6f7e5f0e4dec').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator)
 root.render(
   <React.StrictMode>
     <App />
