a4133659c00c50769745311e98f89c7c39700405
brlopez192 <brlopez192@gmail.com>
2025-02-22 02:34:09 -0500

    chart changes

--- src/components/CityEventsChart.jsx
+++ src/components/CityEventsChart.jsx
@@ -13,7 +13,7 @@ import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip,
     const getData = () => {
         const data = allLocations.map((location) => {
             const count = events.filter((event) => event.location === location).length
-            const city = location.split(', ')[0]
+            const city = location.split((/, | - /))[0]
             return { city, count };
         })
         return data
@@ -25,12 +25,13 @@ import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip,
             margin={{
                 top: 20,
                 right: 20,
-                bottom: 20,
-                left: 20,
+                bottom: 60,
+                left: -30,
             }}
             >
                 <CartesianGrid />
-                <XAxis type="category" dataKey="city" name="City" />
+                <XAxis type="category" dataKey="city" name="City"
+                angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14}} />
                 <YAxis type="number" dataKey="count" name="Number of events" />
                 <Tooltip cursor={{ strokeDasharray: '3 3'}} />
                 <Scatter name="A school" data={data} fill="#8884d8" />
